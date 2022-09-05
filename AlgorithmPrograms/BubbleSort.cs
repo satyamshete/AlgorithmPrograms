123529094c654d14cbdde56b8144ee59ddf52ab0
satyamshete <shetesatyam99@gmail.com>
2022-09-06 00:05:28 +0530

    [Satyam] Bubble Sort Program

---
+++ AlgorithmPrograms/BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class BubbleSort
+    {
+        public BubbleSort(params string[] arr)
+        {
+
+            int i, j;
+            string temp;
+            Console.Write("\nBefore Bubble sorting the elements are : \n");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (i = 0; i < arr.Length - 1; i++)
+            {
+                for (j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.Write("\n\nAfter Bubble sorting the elements are : \n");
+            for (i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+    }
+}
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -6,13 +6,17 @@
         {
             Console.WriteLine("Algorithm Programs");
             Console.WriteLine("Press 1 for Insertion Sort");
+            Console.WriteLine("Press 2 for Bubble Sort");
             int input = int.Parse(Console.ReadLine());
             switch (input) 
             {
                 case 1:
                     InsertionSort insertionSort = new InsertionSort("Dhoni", "Sehwag", "Virat", "Kartik", "Bhuvi", "Yuzi");
                     break;
-                    default:
+                case 2:
+                    BubbleSort bubbleSort = new BubbleSort("Dhoni", "Sehwag", "Virat", "Kartik", "Bhuvi", "Yuzi");
+                    break;
+                default:
                     Console.WriteLine("Make proper Selection");
                     break ;
             }
