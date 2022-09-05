c375285a2541dead2afa7992f07b9f0955d3a4f0
satyamshete <shetesatyam99@gmail.com>
2022-09-05 23:40:05 +0530

    [Satyam] Insertion Sort Program

---
+++ AlgorithmPrograms/InsertionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class InsertionSort
+    {
+        public InsertionSort(params string[] arr)
+        {
+            Console.Write("\nBefore Insertion sorting the elements appears like : \n");
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            for (var i = 1; i < arr.Length; i++)
+            {
+                var current = arr[i];
+
+                var j = i - 1;
+                while (j >= 0 && arr[j].ToLower().CompareTo(current.ToLower()) > 0)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = current;
+            }
+            Console.Write("\nAfter Insertion sorting the elements appears like : \n");
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithm Programs");
+            Console.WriteLine("Press 1 for Insertion Sort");
+            int input = int.Parse(Console.ReadLine());
+            switch (input) 
+            {
+                case 1:
+                    InsertionSort insertionSort = new InsertionSort("Dhoni", "Sehwag", "Virat", "Kartik", "Bhuvi", "Yuzi");
+                    break;
+                    default:
+                    Console.WriteLine("Make proper Selection");
+                    break ;
+            }
+
+
+
+        }
+    }
+}
